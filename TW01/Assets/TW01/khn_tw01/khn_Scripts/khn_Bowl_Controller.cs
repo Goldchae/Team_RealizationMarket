a62b9a29dc3b40e464de6d5165ebeeeb015fd1d8
Gamddalki <hannahk01@naver.com>
2025-04-20 23:42:58 +0900

    create: khn Scene

---
+++ TW01/Assets/TW01/khn_tw01/khn_Scripts/khn_Bowl_Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class khn_Bowl_Controller : MonoBehaviour
+{
+    public GameObject UI_Controller;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item")
+        {
+            UI_Controller.GetComponent<khn_UI_Controller>().Display_PutCounts();
+            Destroy(other.gameObject);
+        }
+    }
+}
