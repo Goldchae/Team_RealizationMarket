a4e150a715d55718adddaf1f09b54f192c57be49
goldchae <goldchae@goldchaeui-MacBookPro.local>
2025-04-24 08:45:18 +0900

    feat : jec 작업

---
+++ TW01/Assets/TW01/jec_tw01/jec_Scripts/jec_Pick_Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jec_Pick_Controller : MonoBehaviour
+{
+    public GameObject UI;   // Step04 (UI 관련 스크립트를 갖고 있는 게임 오브젝트)
+
+    // clickCounter의 값을 1씩 증가시키는 함수
+    public void Add_Click(GameObject Clone)
+    {
+        int clickCounter = UI.GetComponent<jec_UI_Controller>().GetPickCounts();
+        clickCounter++;
+        print($"{clickCounter} 개의 클론을 클릭했습니다.");
+        Destroy(Clone);
+
+        // Step04 (UI에 내용을 표시하는 스크립트 호출)
+        UI.GetComponent<jec_UI_Controller>().Display_PickCounts(clickCounter);
+    }
+}
